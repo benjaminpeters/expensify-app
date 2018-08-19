eb211450bb95fbfc86a4fea8efe731c5c483ab68
Benjamin <benpeters49@gmail.com>
2018-08-19 15:01:16 -0400

    Working with ES6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,8 @@
+//
+// Object Destructuring
+//
+
+
 // const person = {
 //     name: 'Ben',
 //     age: 28,
@@ -15,13 +20,26 @@
 //     console.log(`Its ${temperature} in ${city}`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName)
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName)
+
+//
+// Array Destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Toronto', 'Ontario', '90210'];
+// const [, city, province = 'Nova Scotia'] = address;
+// console.log(`You are in ${city} ${province}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [menuitem, , medium] = item;
+
+console.log(`A medeium ${menuitem} costs ${medium}`);
